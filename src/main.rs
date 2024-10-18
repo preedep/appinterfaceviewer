a45ca66c6a86d4a3888963916c6ea092dddd5f69
Preedee Ponchevin <preedee.digital@gmail.com>
2024-10-18 18:42:27 +0700

    Switch to actix-web for serving static files
    Replaced basic "Hello, world!" setup with actix-web HttpServer to serve static files. Added actix-files dependency to handle file service operations. This enhances the web server's functionality and prepares the project for more complex future developments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    pretty_env_logger::init();
+    HttpServer::new(||
+            App::new().service(
+                fs::Files::new("/statics", ".").index_file("index.html")
+            )
+    ).bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
